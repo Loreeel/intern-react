20b358f6cbff8547dafbd945c2da54d04bce620d
Loreel <andriy.juravel2000@gmail.com>
2024-10-04 21:00:32 +0300

    first commit

--- src/Component/VacanciesList.jsx
+++ src/Component/VacanciesList.jsx
@@ -5,19 +5,24 @@ import styles from './VacanciesList.module.css'
 const VacanciesList = () => {
 
     const [vacancies, setVacancies] = useState([]);
-    const token = "7905b36eaa48ac2e7df6e532fa336414d2c426169efd8df22f1c6daa574a113ea43d987ac9e46a0cdd5923a1a335a1a45800194ac5b5323d89756ab36c848a5f3c86c02095e0a64a9f263ac24c2f71ff7fc750028669c07c0f72f76a45f809eddd75750d47929720f62d998dd8e7edf78e42f5a5363c75fe1321eb56b6cc820a"
+    const token = "4f908c365962aad36ccd76b6260c32c3fdf66b2d9e7a676ce3214be8e980a5845af70f482c77ebf244d381d1f3707f6c949c835c6dec08cbf3838b92c7acf17611229f6606514e7ad8a0e9844a70e9b96b5824fa95d89f325edf5831197eeae2d8e384a28221116fd10e7e96a7d70d1d0370747e78fc25b00f734a4f6bbf218e"
 
     const config = {
         headers: { Authorization: `Bearer ${token}` }
     };
-
+    const baseUrl = "https://shrouded-wildwood-36572-f8ad95480e14.herokuapp.com/"
     useEffect(() => {
-        axios.get('http://localhost:1337/api/vacancies',config)
+        axios.get(baseUrl+'api/vacancies',config)
             .then(response => setVacancies(response.data.data))
             .catch(error => console.error(error));
     }, []);
 
-    return (<div> {vacancies.map(vacancy => (
-        <div className={styles.vacancyCard} key={vacancy.id}><h2>{vacancy.title}</h2> <p>{vacancy.description}</p></div>))} </div>);
+    return (<> {vacancies.map(vacancy => (
+        <div className={styles.vacancyCard} key={vacancy.id}>
+            <h2>{vacancy.title}</h2><b>{vacancy.location}</b>
+            <p>{vacancy.description}</p>
+            <p><em>{vacancy.salary+"$/month"}</em></p>
+        </div>))}
+    </>);
 }
 export default VacanciesList;
